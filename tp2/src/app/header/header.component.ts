be525a421cb6dccf3004b30ff3ac1388a4d471f0
Alex <apham@iut.univ-paris8.fr>
2025-02-16 00:52:49 +0100

    FF g tout refais mais que sur une page, jcouperai apres. DU COU HEADER OK

--- tp2/src/app/header/header.component.ts
+++ tp2/src/app/header/header.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { LogoComponent } from './logo/logo.component';
-import { TopNavComponent } from './top-nav/top-nav.component';
-import { HeaderTextComponent } from './header-text/header-text.component';
-import { IllustrationComponent } from './illustration/illustration.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [LogoComponent, TopNavComponent, HeaderTextComponent, IllustrationComponent],
+  imports: [],
   templateUrl: './header.component.html',
   styleUrl: './header.component.sass'
 })
