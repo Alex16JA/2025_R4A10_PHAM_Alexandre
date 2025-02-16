c5bed647e7b8778eadb38a1d5d450a04af29eed9
Alex <apham@iut.univ-paris8.fr>
2025-02-16 23:11:40 +0100

    FOOTER OK

--- tp2/src/app/footer/footer-content/footer-help/footer-column/footer-column.component.ts
+++ tp2/src/app/footer/footer-content/footer-help/footer-column/footer-column.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-footer-column',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './footer-column.component.sass'
 })
 export class FooterColumnComponent {
-
+  @Input() columnData: any;
 }
--- tp2/src/app/footer/footer-content/footer-help/footer-help.component.ts
+++ tp2/src/app/footer/footer-content/footer-help/footer-help.component.ts
@@ -9,5 +9,33 @@ import { FooterColumnComponent } from './footer-column/footer-column.component';
   styleUrl: './footer-help.component.sass'
 })
 export class FooterHelpComponent {
-
+  columns = [
+    {
+      title: 'Company',
+      links: [
+        { text: 'About', href: '#' },
+        { text: 'Testimonials', href: '#' },
+        { text: 'Find a doctor', href: '#' },
+        { text: 'Apps', href: '#' }
+      ]
+    },
+    {
+      title: 'Region',
+      links: [
+        { text: 'Indonesia', href: '#' },
+        { text: 'Singapore', href: '#' },
+        { text: 'Hongkong', href: '#' },
+        { text: 'Canada', href: '#' }
+      ]
+    },
+    {
+      title: 'Help',
+      links: [
+        { text: 'Help center', href: '#' },
+        { text: 'Contact support', href: '#' },
+        { text: 'Instructions', href: '#' },
+        { text: 'How it works', href: '#' }
+      ]
+    }
+  ];
 }
