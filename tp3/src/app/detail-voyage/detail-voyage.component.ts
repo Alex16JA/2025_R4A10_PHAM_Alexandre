4956091a0d9020cea4d1d1bb76e27f61323b59e1
Alex <apham@iut.univ-paris8.fr>
2025-03-01 20:26:49 +0100

    Detail Voyage OK mais la redirection fonctionne pas

--- tp3/src/app/detail-voyage/detail-voyage.component.ts
+++ tp3/src/app/detail-voyage/detail-voyage.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { VoyageService } from '../service/voyage.service';
+import { Voyage } from '../models/voyage.model';
+import { CurrencyPipe } from '@angular/common'; 
+import { RouterLink, RouterLinkActive } from '@angular/router'; 
 
 @Component({
   selector: 'app-detail-voyage',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe, RouterLink, RouterLinkActive],
   templateUrl: './detail-voyage.component.html',
-  styleUrl: './detail-voyage.component.scss'
+  styleUrls: ['./detail-voyage.component.scss']
 })
-export class DetailVoyageComponent {
+export class DetailVoyageComponent implements OnInit {
+  voyage: Voyage | undefined;
 
+  constructor(
+    private route: ActivatedRoute,
+    private voyageService: VoyageService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.voyage = this.voyageService.getVoyageById(id);
+    }
+  }
+
+  deleteVoyage(id: string | undefined): void {
+    if (id && confirm('Êtes-vous sûr de vouloir supprimer ce voyage ?')) {
+      this.voyageService.deleteVoyage(id);
+      window.location.href = '/home';
+    }
+  }
 }
