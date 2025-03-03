f4d9ac392ca9abcf38a8a811fec4abcb49d7241e
Alex <apham@iut.univ-paris8.fr>
2025-03-03 23:08:20 +0100

    composant btn-accueil

--- tp3/src/app/btn-accueil/btn-accueil.component.ts
+++ tp3/src/app/btn-accueil/btn-accueil.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-btn-accueil',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './btn-accueil.component.scss'
 })
 export class BtnAccueilComponent {
+  constructor(private router: Router) { }
 
+  navigateToHome() {
+    this.router.navigate(['/home']);
+  }
 }
--- tp3/src/app/not-found/not-found.component.ts
+++ tp3/src/app/not-found/not-found.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { BtnAccueilComponent } from '../btn-accueil/btn-accueil.component';
 
 @Component({
   selector: 'app-not-found',
   standalone: true,
-  imports: [],
+  imports: [BtnAccueilComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
