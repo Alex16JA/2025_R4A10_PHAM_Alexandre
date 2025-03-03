5bf00872d317d017b03513bfa18aedec2294f667
Alex <apham@iut.univ-paris8.fr>
2025-03-03 22:14:44 +0100

    pagination

--- tp3/src/app/home/home.component.ts
+++ tp3/src/app/home/home.component.ts
@@ -13,15 +13,31 @@ import { SlicePipe } from '@angular/common';
 })
 export class HomeComponent {
   voyages: Voyage[] = [];
+  currentPage: number = 1;
+  itemsPerPage: number = 20;
+  totalItems: number = 0;
+  Math = Math; 
 
   constructor(private voyageService: VoyageService) {
     this.voyages = this.voyageService.getVoyages();
+    this.totalItems = this.voyages.length;
+  }
+
+  get pagination(): Voyage[] {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.voyages.slice(startIndex, endIndex);
+  }
+
+  changePage(page: number): void {
+    this.currentPage = page;
   }
 
   deleteVoyage(id: string): void {
     if (confirm('Êtes-vous sûr de vouloir supprimer ce voyage ?')) {
       this.voyageService.deleteVoyage(id);
       this.voyages = this.voyageService.getVoyages();
+      this.totalItems = this.voyages.length;
     }
   }
 }
