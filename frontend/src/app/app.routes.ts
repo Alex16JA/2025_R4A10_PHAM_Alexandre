4a1e9545ae22bbff559d06b1e0731d1b650b59bd
Alex <apham@iut.univ-paris8.fr>
2025-03-04 11:23:31 +0100

    importation du bon tp

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
-import { Routes } from '@angular/router';
 import { BookListPageComponent } from './pages/book-list-page/book-list-page.component';
 import { SingleBookPageComponent } from './pages/single-book-page/single-book-page.component';
 import { CreateBookPageComponent } from './pages/create-book-page/create-book-page.component';
+import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   { path: '', component: BookListPageComponent },
--- frontend/src/app/pages/create-book-page/create-book-page.component.ts
+++ frontend/src/app/pages/create-book-page/create-book-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { BooksInMemoryService } from '../../services/book-inmemory.service';
+import { Router } from '@angular/router';
+import { Book } from '../../models/book';
 
 @Component({
   selector: 'app-create-book-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-book-page.component.html',
   styleUrl: './create-book-page.component.css',
 })
-export class CreateBookPageComponent {}
+export class CreateBookPageComponent {
+  @Output() bookCreationEventEmitter = new EventEmitter<Book>();
+
+  bookForm = new FormGroup({
+    titre: new FormControl('', [Validators.required,Validators.minLength(2),Validators.maxLength(35),]),
+    auteur: new FormControl('', [Validators.required,Validators.minLength(5),Validators.maxLength(50),]),
+    description: new FormControl('', [Validators.required,Validators.minLength(10),Validators.maxLength(255),]),
+  });
+
+  onSubmit() {
+    if (this.bookForm.valid) {
+      this.bookCreationEventEmitter.emit(this.bookForm.value as Book);
+    }
+  }
+}
