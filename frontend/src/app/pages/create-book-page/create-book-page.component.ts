537e9c12656547201dbc7f8b34f58356d6294c74
Alex <apham@iut.univ-paris8.fr>
2025-03-04 11:35:50 +0100

    TP OK

--- frontend/src/app/pages/create-book-page/create-book-page.component.ts
+++ frontend/src/app/pages/create-book-page/create-book-page.component.ts
@@ -20,14 +20,22 @@ export class CreateBookPageComponent {
   @Output() bookCreationEventEmitter = new EventEmitter<Book>();
 
   bookForm = new FormGroup({
-    titre: new FormControl('', [Validators.required,Validators.minLength(2),Validators.maxLength(35),]),
-    auteur: new FormControl('', [Validators.required,Validators.minLength(5),Validators.maxLength(50),]),
+    title: new FormControl('', [Validators.required,Validators.minLength(2),Validators.maxLength(35),]),
+    author: new FormControl('', [Validators.required,Validators.minLength(5),Validators.maxLength(50),]),
     description: new FormControl('', [Validators.required,Validators.minLength(10),Validators.maxLength(255),]),
   });
 
+  constructor(
+    private bookService: BooksInMemoryService,
+    private router: Router
+  ) {}
+
   onSubmit() {
     if (this.bookForm.valid) {
-      this.bookCreationEventEmitter.emit(this.bookForm.value as Book);
+      const newBook = this.bookForm.value as Omit<Book, 'id' | 'coverUrl'>;
+      const bookId = this.bookService.createBook(newBook);
+      this.bookCreationEventEmitter.emit({ ...newBook, id: bookId, coverUrl: 'https://placehold.co/150x200' });
+      this.router.navigate(['/book', bookId]);
     }
   }
 }
